6a28e1ddf2b51d9442cd083bbb822ba20a63a903
Anya <likamoon@list.ru>
2020-10-29 01:48:28 +0300

    Блоки на главной

--- src/js/script.js
+++ src/js/script.js
@@ -15,6 +15,30 @@ $(document).ready(function () {
         }
     }
 
+    //Промо-слайдер
+    $('#promo-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        appendDots: $('.promo-slider__dots'),
+        rows: 0,
+        prevArrow: '#promo-slider-prev',
+        nextArrow: '#promo-slider-next',
+        speed: 1000,
+        fade: true,
+        cssEase: 'linear',
+
+        responsive: [            
+            {
+                breakpoint: 670,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+    });
+
 })
 
 /*Полифилы для ie*/
--- www/js/script.js
+++ www/js/script.js
@@ -14,7 +14,28 @@ $(document).ready(function () {
 
     burger.addEventListener('click', toggleBurger);
     var menuBlock = document.querySelector('.menu-block');
-  }
+  } //Промо-слайдер
+
+
+  $('#promo-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    appendDots: $('.promo-slider__dots'),
+    rows: 0,
+    prevArrow: '#promo-slider-prev',
+    nextArrow: '#promo-slider-next',
+    speed: 1000,
+    fade: true,
+    cssEase: 'linear',
+    responsive: [{
+      breakpoint: 670,
+      settings: {
+        arrows: false
+      }
+    }]
+  });
 });
 /*Полифилы для ie*/
 
